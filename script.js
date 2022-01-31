cdbab77cb2d3e1ba37b60747c56058c633f70b14
Herbert <herbert.squeiroz@gmail.com>
2022-01-31 20:35:22 -0300

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function abriromenu() {
+  document.getElementById('menu').style.width = '300px'
+  document.getElementById('conteudo').style.marginLeft = '300px'
+}
+
+function fecharomenu() {
+  document.getElementById('menu').style.width = '0px'
+  document.getElementById('conteudo').style.marginLeft = '0px'
+}
